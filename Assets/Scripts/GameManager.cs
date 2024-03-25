c58f7b92bd4d038b4fe24f6ed481ada5821d7b37
Juan Diego Jaramillo <juan.jaramillo@uees.edu.ec>
2024-03-24 23:15:42 -0500

    Nivel
    nivel hecho por Juan Diego Jaramillo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int scoreCoin;
+    public int scoreEnemy;
+    public Text textScore;
+    public Text textVida;
+
+    //private PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreCoin = 0;
+        scoreEnemy = 0;
+        textScore.text = "Coins: " + scoreCoin + "   Enemies: " + scoreEnemy ;
+        textVida.text = "Vida: 100";
+    }
+
+    public void AddScoreCoins()
+    {
+        scoreCoin += 1;
+        textScore.text = "Coins: " + scoreCoin + "   Enemies: " + scoreEnemy;
+    }
+
+    public void AddScoreEnemies()
+    {
+        scoreEnemy += 1;
+        textScore.text = "Coins: " + scoreCoin + "   Enemies: " + scoreEnemy;
+    }
+
+    public void RestaVida(){
+
+        textVida.text = "Vida: " + gameObject.GetComponent<PlayerMovement>().vida;
+    }
+}
